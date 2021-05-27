c242970952ac0b16477bd5aad2b12f1588ea271c
anthon70 <anthon70@CRATER>
2021-05-27 06:26:02 -0700

    Initial commit. Can draw a polygon and generate a form.

---
+++ GeoenrichmentTool/PolygonSelect.cs
@@ -0,0 +1,47 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeoenrichmentTool
+{
+    internal class PolygonSelect : MapTool
+    {
+        protected Geometry currentGeometry;
+
+        public PolygonSelect()
+        {
+            IsSketchTool = true;
+            SketchType = SketchGeometryType.Polygon;
+            SketchOutputMode = SketchOutputMode.Map;
+        }
+
+        protected override Task OnToolActivateAsync(bool active)
+        {
+            return base.OnToolActivateAsync(active);
+        }
+
+        protected override Task<bool> OnSketchCompleteAsync(Geometry geometry)
+        {
+            currentGeometry = geometry;
+            Form geoForm = new GeoSPARQL_Query();
+            geoForm.ShowDialog();
+            return base.OnSketchCompleteAsync(geometry);
+        }
+    }
+}
