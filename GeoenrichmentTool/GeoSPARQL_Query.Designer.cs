9983dc55070c3c204d8b873aaefb130ed0e5ea21
anthon70 <anthon70@CRATER>
2021-05-28 05:46:10 -0700

    Added most of the form elements

--- GeoenrichmentTool/GeoSPARQL_Query.Designer.cs
+++ GeoenrichmentTool/GeoSPARQL_Query.Designer.cs
@@ -29,110 +29,155 @@ namespace GeoenrichmentTool
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.endPointLabel = new System.Windows.Forms.Label();
+            this.endPoint = new System.Windows.Forms.TextBox();
+            this.geoFormName = new System.Windows.Forms.Label();
+            this.placeTypeLabel = new System.Windows.Forms.Label();
+            this.placeType = new System.Windows.Forms.TextBox();
+            this.subclassReasoning = new System.Windows.Forms.CheckBox();
+            this.calculatorLabel = new System.Windows.Forms.Label();
+            this.calculator = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultFolderLabel = new System.Windows.Forms.Label();
+            this.classNameLabel = new System.Windows.Forms.Label();
+            this.className = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // endPointLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 95);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "GeoSPARQL Endpoint:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.endPointLabel.AutoSize = true;
+            this.endPointLabel.Location = new System.Drawing.Point(15, 95);
+            this.endPointLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.endPointLabel.Name = "endPointLabel";
+            this.endPointLabel.Size = new System.Drawing.Size(196, 20);
+            this.endPointLabel.TabIndex = 0;
+            this.endPointLabel.Text = "GeoSPARQL Endpoint:";
             // 
-            // textBox1
+            // endPoint
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(19, 118);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(740, 26);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "http://stko-roy.geog.ucsb.edu:7202/repositories/plume_soil_wildfire";
+            this.endPoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.endPoint.Location = new System.Drawing.Point(19, 118);
+            this.endPoint.Name = "endPoint";
+            this.endPoint.Size = new System.Drawing.Size(740, 26);
+            this.endPoint.TabIndex = 1;
+            this.endPoint.Text = "http://stko-roy.geog.ucsb.edu:7202/repositories/plume_soil_wildfire";
             // 
-            // label2
+            // geoFormName
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(316, 37);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "GeoSPARQL Query";
+            this.geoFormName.AutoSize = true;
+            this.geoFormName.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.geoFormName.Location = new System.Drawing.Point(12, 9);
+            this.geoFormName.Name = "geoFormName";
+            this.geoFormName.Size = new System.Drawing.Size(316, 37);
+            this.geoFormName.TabIndex = 2;
+            this.geoFormName.Text = "GeoSPARQL Query";
             // 
-            // label3
+            // placeTypeLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 147);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Place Type:";
+            this.placeTypeLabel.AutoSize = true;
+            this.placeTypeLabel.Location = new System.Drawing.Point(15, 147);
+            this.placeTypeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.placeTypeLabel.Name = "placeTypeLabel";
+            this.placeTypeLabel.Size = new System.Drawing.Size(101, 20);
+            this.placeTypeLabel.TabIndex = 3;
+            this.placeTypeLabel.Text = "Place Type:";
             // 
-            // textBox2
+            // placeType
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(19, 170);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(740, 26);
-            this.textBox2.TabIndex = 4;
+            this.placeType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.placeType.Location = new System.Drawing.Point(19, 170);
+            this.placeType.Name = "placeType";
+            this.placeType.Size = new System.Drawing.Size(740, 26);
+            this.placeType.TabIndex = 4;
             // 
-            // checkBox1
+            // subclassReasoning
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 203);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(348, 24);
-            this.checkBox1.TabIndex = 5;
-            this.checkBox1.Text = "Disable Transistive Subclass Reasoning";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.subclassReasoning.AutoSize = true;
+            this.subclassReasoning.Location = new System.Drawing.Point(19, 203);
+            this.subclassReasoning.Name = "subclassReasoning";
+            this.subclassReasoning.Size = new System.Drawing.Size(348, 24);
+            this.subclassReasoning.TabIndex = 5;
+            this.subclassReasoning.Text = "Disable Transistive Subclass Reasoning";
+            this.subclassReasoning.UseVisualStyleBackColor = true;
             // 
-            // label4
+            // calculatorLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 230);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(228, 20);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Spatial Relation Calculator:";
+            this.calculatorLabel.AutoSize = true;
+            this.calculatorLabel.Location = new System.Drawing.Point(15, 230);
+            this.calculatorLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.calculatorLabel.Name = "calculatorLabel";
+            this.calculatorLabel.Size = new System.Drawing.Size(228, 20);
+            this.calculatorLabel.TabIndex = 6;
+            this.calculatorLabel.Text = "Spatial Relation Calculator:";
             // 
-            // comboBox1
+            // calculator
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.calculator.FormattingEnabled = true;
+            this.calculator.Items.AddRange(new object[] {
             "Contain + Intersect",
             "Contain",
             "Within",
             "Intersect"});
-            this.comboBox1.Location = new System.Drawing.Point(19, 254);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(740, 28);
-            this.comboBox1.TabIndex = 7;
+            this.calculator.Location = new System.Drawing.Point(19, 254);
+            this.calculator.Name = "calculator";
+            this.calculator.Size = new System.Drawing.Size(740, 28);
+            this.calculator.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // resultFolderLabel
+            // 
+            this.resultFolderLabel.AutoSize = true;
+            this.resultFolderLabel.Location = new System.Drawing.Point(15, 285);
+            this.resultFolderLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultFolderLabel.Name = "resultFolderLabel";
+            this.resultFolderLabel.Size = new System.Drawing.Size(169, 20);
+            this.resultFolderLabel.TabIndex = 9;
+            this.resultFolderLabel.Text = "Query Result Folder";
+            // 
+            // classNameLabel
+            // 
+            this.classNameLabel.AutoSize = true;
+            this.classNameLabel.Location = new System.Drawing.Point(15, 305);
+            this.classNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.classNameLabel.Name = "classNameLabel";
+            this.classNameLabel.Size = new System.Drawing.Size(172, 20);
+            this.classNameLabel.TabIndex = 10;
+            this.classNameLabel.Text = "Feature Class Name";
+            // 
+            // className
+            // 
+            this.className.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.className.Location = new System.Drawing.Point(19, 328);
+            this.className.Name = "className";
+            this.className.Size = new System.Drawing.Size(740, 26);
+            this.className.TabIndex = 11;
             // 
             // GeoSPARQL_Query
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1333, 692);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(778, 465);
+            this.Controls.Add(this.className);
+            this.Controls.Add(this.classNameLabel);
+            this.Controls.Add(this.resultFolderLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.calculator);
+            this.Controls.Add(this.calculatorLabel);
+            this.Controls.Add(this.subclassReasoning);
+            this.Controls.Add(this.placeType);
+            this.Controls.Add(this.placeTypeLabel);
+            this.Controls.Add(this.geoFormName);
+            this.Controls.Add(this.endPoint);
+            this.Controls.Add(this.endPointLabel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "GeoSPARQL_Query";
@@ -145,13 +190,17 @@ namespace GeoenrichmentTool
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label endPointLabel;
+        private System.Windows.Forms.TextBox endPoint;
+        private System.Windows.Forms.Label geoFormName;
+        private System.Windows.Forms.Label placeTypeLabel;
+        private System.Windows.Forms.TextBox placeType;
+        private System.Windows.Forms.CheckBox subclassReasoning;
+        private System.Windows.Forms.Label calculatorLabel;
+        private System.Windows.Forms.ComboBox calculator;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resultFolderLabel;
+        private System.Windows.Forms.Label classNameLabel;
+        private System.Windows.Forms.TextBox className;
     }
 }
--- GeoenrichmentTool/GeoSPARQL_Query.cs
+++ GeoenrichmentTool/GeoSPARQL_Query.cs
@@ -21,10 +21,5 @@ namespace GeoenrichmentTool
         {
 
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
