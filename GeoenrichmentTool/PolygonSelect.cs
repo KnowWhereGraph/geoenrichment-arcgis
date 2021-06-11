527dd7241b8fff3860c6f3cb4a6722e916caa852
anthon70 <anthon70@CRATER>
2021-06-11 08:42:24 -0700

    Submitting some comments with different things I tried for drawing polygons on the map

--- GeoenrichmentTool/PolygonSelect.cs
+++ GeoenrichmentTool/PolygonSelect.cs
@@ -36,14 +36,37 @@ namespace GeoenrichmentTool
 
         protected override Task<bool> OnSketchCompleteAsync(Geometry geometry)
         {
-            var mv = MapView.Active;
+            MapView mv = MapView.Active;
+            //bool zoom = mv.ZoomToAsync(geometry).Result;
 
             //TODO::Turn geometry into a feature class layer
-            QueuedTask.Run<GroupLayer>(() =>
-            {
-                return LayerFactory.Instance.CreateGroupLayer(mv.Map, 0, "GeoSPARQL Layer");
-            });
+            //GroupLayer groupLayer = QueuedTask.Run<GroupLayer>(() =>
+            //{
+            //    return LayerFactory.Instance.CreateGroupLayer(mv.Map, 0, "GeoSPARQL Layer");
+            //}).Result;
+
+            //var features = QueuedTask.Run<Dictionary<BasicFeatureLayer,List<long>>>(() =>
+            //{
+            //    return mv.GetFeatures(geometry);
+            //}).Result;
+
+            //var overlay = QueuedTask.Run<IDisposable>(() =>
+            //{
+            //    return mv.AddOverlay(geometry);
+            //}).Result;
+
+            //GDBProjectItem activeGDB = Project.Current.GetItems<GDBProjectItem>().First();
+            //string path = activeGDB.Path;
+            //Uri uri = new Uri(path);
+            //FeatureClass fc = new FeatureClass();
+            //FeatureLayer featureLayer = QueuedTask.Run<FeatureLayer>(() =>
+            //{
+            //return LayerFactory.Instance.CreateFeatureLayer("", groupLayer);
+            //}).Result;
+
+            //bool test = CreateFeatureAsync(mv, geometry).Result;
             
+
             Form geoForm = new GeoSPARQL_Query(geometry);
             geoForm.ShowDialog();
             return base.OnSketchCompleteAsync(geometry);
